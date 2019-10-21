4abec6d0195230529046bcd384d4206464c0cdfa
axmitchell <axmitchell202@gmail.com>
2019-10-21 10:21:17 -0400

    Add random fireworks that disappear after drawn

--- fireworks.js
+++ fireworks.js
@@ -1,146 +1,117 @@
+//set trigger to spacebar
+document.body.onkeyup = function(e){
+    if (e.keyCode == 32){
+        explode()
+    }
+};
+
+//create canvas
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
-document.getElementsByTagName('button')[0].addEventListener('click', ignite);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight
 
-let width = 1000
-let height = 500
-let centerWidth = width/2
-let centerHeight = height/2
-let trailLeft = 500
-let trailRight = 500
-let trailUp = 250
-let trailDown = 250
-let cell = 25
-
-// draw sky 
+//create sky
 ctx.fillStyle = "darkblue";
-ctx.fillRect(0,0,width,height);
+ctx.fillRect(0,0,canvas.width,canvas.height); 
 
-// draw firework cell
-ctx.fillStyle = "brown";
-ctx.fillRect((width/2),(height-cell),cell,cell);
+//set firework parameters
+let maxSize = 25
+let path = 1
+let explosionsLimit = 6
 
-function ignite() {
-    ctx.fillStyle = "red";
-    ctx.fillRect((width/2),(height-cell),cell,cell);
-    launch()
+//set random values for each firework
+let x = Math.floor(Math.random()*canvas.width)
+let y = Math.floor(Math.random()*canvas.height)
+let size = Math.floor(Math.random()*maxSize)
+
+//create array to hold fireworks' values
+let pastExplosions = []
+
+//create firework pixels
+function drawFirework(fx,fy,fs,color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(fx,fy,fs,fs)
 }
 
-function launch () {
-    setTimeout(function(){
-        if (height>=(500/2)) {
-            ctx.fillStyle = "red";
-            ctx.fillRect((width/2),(height-cell),cell,cell);
-            ctx.fillStyle = "darkblue";
-            ctx.fillRect((width/2),(height),cell,cell);
-            height-=cell;
-            console.log(height);
-            launch();
-        }
-        else {
-            ctx.fillStyle = "orange";
-            ctx.fillRect((width/2),(500/2)-cell,cell,cell);
-            explode()
-        
-        }
-    }, 100);
-}
-
+//create fireworks
 function explode() {
     setTimeout(function(){
-        if (height>=(500/8)) {
-
-            //up
-            ctx.fillStyle = "yellow";
-            ctx.fillRect(centerWidth+(cell/4),trailUp-cell*2,cell/2,cell/2);
-
-            //down
-            ctx.fillStyle = "yellow";
-            ctx.fillRect(centerWidth+(cell/4),trailDown+(cell/2),cell/2,cell/2);
-
-            //left
-            ctx.fillStyle = "yellow";
-            ctx.fillRect(trailLeft-(cell),centerHeight-(cell*.75),cell/2,cell/2);
-
-            //right
-            ctx.fillStyle = "yellow";
-            ctx.fillRect(trailRight+(cell*1.5),centerHeight-(cell*.75),cell/2,cell/2);
-
-            //top-right
-            ctx.fillStyle = "purple";
-            ctx.fillRect(trailRight+(cell/4),trailUp-(cell*.75),cell/2,cell/2);
-
-            //top-left
-            ctx.fillStyle = "purple";
-            ctx.fillRect(trailLeft+(cell/4),trailUp-(cell*.75),cell/2,cell/2);
-
-            //bottom-right
-            ctx.fillStyle = "purple";
-            ctx.fillRect(trailRight+(cell/4),trailDown-(cell*.75),cell/2,cell/2);
-
-            //bottom-left
-            ctx.fillStyle = "purple";
-            ctx.fillRect(trailLeft+(cell/4),trailDown-(cell*.75),cell/2,cell/2);
-
-            trailUp-=cell;
-            trailLeft-=cell;
-            trailRight+=cell;
-            trailDown+=cell;
-            height-=cell
-
-            explode();
-        } 
-        else {
-            height = 500;
-            randomFirework()
+        if (path < size*6) {
+            drawFirework(x,y-size,size, getRandomColor()); //draw center
+            drawFirework(x+(size/4),y-path-(2*size),size/2, getRandomColor()); //draw top path
+            drawFirework(x+path+(size*1.25),y-path-(size*1.75),size/2,getRandomColor()); //draw top-right path
+            drawFirework(x+path+(size*1.5),y-(3*size/4),size/2,getRandomColor()); //draw right path
+            drawFirework(x+path+(size*1.25),y+path+(size/4),size/2,getRandomColor()); //draw bottom-right path
+            drawFirework(x+(size/4),y+path+(size/2),size/2,getRandomColor()); //draw down path
+            drawFirework(x-path-(3*size/4),y+path+(size/4),size/2,getRandomColor());  //draw bottom-left path
+            drawFirework(x-path-size,y-(3*size/4),size/2,getRandomColor()); //draw left path
+            drawFirework(x-path-(3*size/4),y-path-(size*1.75),size/2,getRandomColor()); //draw top-left path 
+            
+            //add firework's values to array
+            pastExplosions.push([x,y,size])   
+            
+            path += size;
+            explode()
         }
-    }, 200);
-    
+        else if (explosionsLimit>1) {
+            //reset values
+            path = 1
+            x = Math.floor(Math.random()*canvas.width)
+            y = Math.floor(Math.random()*canvas.height)
+            size = Math.floor(Math.random()*maxSize)
+            
+            console.log(pastExplosions[pastExplosions.length-1])
+            explosionsLimit--
+            explode(); 
+            fadeFireworks()
+        }  
+        else {
+            explosionsLimit = 6
+            fadeFireworks()
+        }
+    }, 150);
 }
 
-function randomFirework() {
-    let randomHeight = Math.floor(Math.random()*height);
-    let randomWidth = Math.floor(Math.random()*width);
-    let randomUp = randomHeight;
-    let randomDown = randomHeight;
-    let randomLeft = randomWidth;
-    let randomRight = randomWidth;
-    let randomCenter = randomHeight;
-    ctx.fillStyle = "orange";
-    ctx.fillRect(randomWidth, randomHeight, cell/2, cell/2);
-    console.log(randomWidth + " " + randomHeight);
-    randomExplosion(randomWidth, randomHeight, randomUp, randomDown, randomLeft, randomRight, randomCenter);
+//remove fireworks
+function fadeFireworks() {
+    let pastExplosion = pastExplosions[0]
+    let oldX = pastExplosion[0]
+    let oldY = pastExplosion[1]
+    let oldSize = pastExplosion[2]
+        if (path < oldSize*6) {
+            drawFirework(oldX,oldY-oldSize,oldSize, 'darkblue'); //draw center
+            drawFirework(oldX+(oldSize/4),oldY-path-(2*oldSize),oldSize/2,'darkblue'); //draw top path
+            drawFirework(oldX+path+(oldSize*1.25),oldY-path-(oldSize*1.75),oldSize/2,'darkblue'); //draw top-right path
+            drawFirework(oldX+path+(oldSize*1.5),oldY-(3*oldSize/4),oldSize/2,'darkblue'); //draw right path
+            drawFirework(oldX+path+(oldSize*1.25),oldY+path+(oldSize/4),oldSize/2,'darkblue'); //draw bottom-right path
+            drawFirework(oldX+(oldSize/4),oldY+path+(oldSize/2),oldSize/2,'darkblue'); //draw bottom path
+            drawFirework(oldX-path-(3*oldSize/4),oldY+path+(oldSize/4),oldSize/2,'darkblue'); //draw bottom-left path
+            drawFirework(oldX-path-oldSize,oldY-(3*oldSize/4),oldSize/2,'darkblue'); //draw left path
+            drawFirework(oldX-path-(3*oldSize/4),oldY-path-(oldSize*1.75),oldSize/2,'darkblue'); //draw top-left path     
+            path += oldSize;            
+            fadeFireworks();
+        }
+        else{    
+            //reset values
+            path = 1
+            oldX = pastExplosion[0]
+            oldY = pastExplosion[1]
+            oldSize = pastExplosion[2]
+            
+            //remove first item from array
+            pastExplosions.shift()
+            
+            fadeFireworks();
+        }
 }
 
-function randomExplosion(randomW, randomH, randomU, randomD, randomL, randomR, randomC) {
-    setTimeout(function(){
-    if (randomC<=randomH+200) {
-
-    //random up
-    ctx.fillStyle = 'yellow'
-    ctx.fillRect(randomW, randomU, cell/4, cell/4)
-
-    //random down
-    ctx.fillStyle = 'yellow'
-    ctx.fillRect(randomW, randomD, cell/4, cell/4)
-    
-    //random left
-    ctx.fillStyle = 'yellow'
-    ctx.fillRect(randomL, randomH, cell/4, cell/4)
-
-    //random right
-    ctx.fillStyle = 'yellow'
-    ctx.fillRect(randomR, randomH, cell/4, cell/4)
-
-    randomU+=cell/4
-    randomD-=cell/4
-    randomL-=cell/4
-    randomR+=cell/4
-    randomC+=cell
-    randomExplosion(randomW, randomH, randomU, randomD, randomL, randomR, randomC);
-    } else {
-        randomC = randomH;
-        randomFirework();
+//return random color
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
     }
-}, 120)
+    return color;
 }
